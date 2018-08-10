6d029d98b877f9abf10d78d20e2e2f4cefdbe340
carolmbithe <carombithe97@gmail.com>
2018-08-10 12:48:27 +0300

    prompt user's input

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+var sideOne=parseInt(prompt("Enter side One"));
+var sideTwo=parseInt(prompt("Enter side Two"));
+var sideThree=parseInt(prompt("Enter side Three"))
+if (sideOne ===sideTwo &&sideThree===sideTwo) {
+alert("It is a equilateral triangle");
+} else if (sideOne===sideTwo ||sideTwo===sideThree || sideThree===sideOne) {
+alert("It is an isosceles triangle");
+}else if (sideOne !==sideTwo || sideTwo !==sideThree ||sideThree !==sideOne) {
+alert("Its a scalene triangle");
+} else
+alert("It's not a triangle");{
+}
