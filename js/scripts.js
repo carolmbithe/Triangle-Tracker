51ce42b106b6a82ff73864f120270e8ec72a1751
carolmbithe <carombithe97@gmail.com>
2018-08-10 14:06:37 +0300

    used control flows and logical operators

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
 var sideOne=parseInt(prompt("Enter side One"));
 var sideTwo=parseInt(prompt("Enter side Two"));
 var sideThree=parseInt(prompt("Enter side Three"))
-if (sideOne ===sideTwo &&sideThree===sideTwo) {
+if (sideOne+sideTwo<=sideThree || sideTwo+sideThree<=sideOne ||sideOne+sideThree<=sideTwo){
+alert("It's not a triangle");
+}
+else if (sideOne ===sideTwo &&sideThree===sideTwo) {
 alert("It is a equilateral triangle");
 } else if (sideOne===sideTwo ||sideTwo===sideThree || sideThree===sideOne) {
 alert("It is an isosceles triangle");
-}else if (sideOne !==sideTwo || sideTwo !==sideThree ||sideThree !==sideOne) {
+} else if (sideOne !==sideTwo || sideTwo !==sideThree ||sideThree !==sideOne) {
 alert("Its a scalene triangle");
-} else
-alert("It's not a triangle");{
-}
+ }
